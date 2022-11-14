860ea9467230b6ed8a52abe8e833add0120571a9
Sergei Rodionov <rodser92@gmail.com>
2022-11-14 23:24:16 +0300

    Add print name and password user from json

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from datetime import datetime
 
 date_release = str(datetime.now().strftime('%d/%m/%y'))
 print("Testing project for Git \n" + ' by Rodionov Sergei \n' + '\t' + date_release)
+
+import json
+
+with open('account.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+print('Имя пользователя:' + '\t' + data['user_name'] + '\n'
+      'Пароль:' + '\t' + data['password'])
+
