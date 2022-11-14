36f20a6087cec03584115845e7b2dafc7b842967
Sergei Rodionov <rodser92@gmail.com>
2022-11-14 23:04:29 +0300

    Add date for print message

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Testing project for Git \n" + ' by Rodionov Sergei')
+from datetime import datetime
+
+date_release = str(datetime.now().strftime('%d/%m/%y'))
+print("Testing project for Git \n" + ' by Rodionov Sergei \n' + '\t' + date_release)
